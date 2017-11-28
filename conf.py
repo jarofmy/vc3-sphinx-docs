798673414e4d9894d8bcd59bf8a5717cd3832d2e
Jaro <jeremyvan614@gmail.com>
2017-11-28 12:36:50 -0600

    Revert back to Working Doc

--- conf.py
+++ conf.py
@@ -95,10 +95,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_context = {
-    'css_files': ['_static/theme_overrides.css',  # overrides for RTD theme
-                  ],
-}
+# html_context = {
+#     'css_files': ['_static/css/theme_overrides.css',  # overrides for RTD theme
+#                   ],
+# }
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Custom sidebar templates, must be a dictionary that maps document names
